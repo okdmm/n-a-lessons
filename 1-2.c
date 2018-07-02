361ca4e00dee566dffea8dd8534df45444357eae
okada <kirinnremonn1122@gmail.com>
2018-07-02 12:01:22 +0900

    report2

--- 1-2.c
+++ 1-2.c
@@ -4,10 +4,10 @@
 int main(){
   double x,y;
   double f,g;
-  y = 0.0000004;
-  x = 1.0;
-  f = 1.0/(sqrt(x+y) - sqrt(x));
-  printf("%.15f\n", sqrt(x+y)-sqrt(x));
+  y = 0.000001;
+  x = 10000000.0;
+  f = 1/(sqrt(x+y) - sqrt(x));
+  printf("1/f = %.24f\n", sqrt(x+y)-sqrt(x));
   g = (sqrt(x+y) + sqrt(x))/y;
   printf("f = %f\n",f);
   printf("g = %f\n",g);
--- 2-1.c
+++ 2-1.c
@@ -5,7 +5,7 @@
 double p(x)
      double x;
  {  double y;
-  y =  cos(x)+(x+1)*exp(x);
+  y =  x*x*x + x*x - 5*x + 3;
   return (y);
     }
 
@@ -13,19 +13,30 @@ double dp(x)
      double x;
 {
   double y;
-  y = -sin(x)+exp(x)+(x+1)*exp(x);
+  y = 3*x*x + 2*x - 5;
   return (y);
 }
 
-main(){
+int main(){
 
 
   
   double x, xx, tmp, eps;
   int k, kmax;
   kmax=50;
-  eps = 1.e-15;
-  x =1.0;  /*  x=1.5; */
+  eps = 1.e-12;
+  x =30;
+  for (k=1; k <= kmax; ++k){
+    tmp= p(x)/dp(x);
+    xx = x - tmp;
+    if (fabs(tmp) <= eps) break;
+    else
+      x = xx;
+      printf("%5d    %21.17e\n", k, xx);
+  }
+   printf("%5d    %21.17e\n", k, xx);
+
+  x = -30;
   for (k=1; k <= kmax; ++k){
     tmp= p(x)/dp(x);
     xx = x - tmp;
@@ -36,4 +47,3 @@ main(){
   }
    printf("%5d    %21.17e\n", k, xx);
  }
-        
